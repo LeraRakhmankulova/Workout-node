b986f74bd2c95e1d1c327763651576f873820ca9
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-10-11 17:54:59 +0300

    Update tokenModel.js

--- back/models/tokenModel.js
+++ back/models/tokenModel.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
+
 const Schema = mongoose.Schema;
 
-const tokenShema = mongoose.Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  refreshToken: { type: String, required: true },
-},  {
-  minimize: false,
-  timestamps: true,
-});
+const tokenShema = mongoose.Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    refreshToken: { type: String, required: true },
+  },
+  {
+    minimize: false,
+    timestamps: true,
+  }
+);
 
 const Token = mongoose.model("Token", tokenShema);
 
