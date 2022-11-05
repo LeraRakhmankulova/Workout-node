735fd0b89e26673bc38e67e18b95cf808f568aff
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-05 22:11:16 +0300

    fix

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,17 @@
-import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/homePage/Home';
-import styles from './App.module.sass'
-import Login from './pages/authPage/Login';
-import Registration from './pages/authPage/Registration';
+import Login from "./pages/authPage/Login";
+import Registration from "./pages/authPage/Registration";
+import Home from "./pages/homePage/Home";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className={styles.main}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/reg" element={<Registration />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reg" element={<Registration />} />
+      </Routes>
+    </Router>
   );
 };
 
--- front/src/pages/authPage/Registration.jsx
+++ front/src/pages/authPage/Registration.jsx
@@ -4,8 +4,19 @@ import back from '../../assets/reg.png'
 const Registration = () => {
     return (
         <div className={styles.reg}>
-            <div className={styles.reg__back}>
-                <img src={back} alt="sportsman" />
+            <div className={styles.reg__info}>
+                <div className={styles.reg__back}>
+                    <img src={back} alt="sportsman" />
+                </div>
+                <ul className={styles.reg__head}>
+                    <li>Login</li>
+                    <li>Sign up</li>
+                </ul>
+            </div>
+            <div>
+                <div>
+                    <input />
+                </div>
             </div>
         </div>
     )
